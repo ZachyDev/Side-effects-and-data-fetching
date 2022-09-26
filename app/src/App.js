43b6c51da2d59b27b7e70e8077b065f4c5e6c221
= <=>
2022-09-27 02:55:23 +0300

    :rocket: fetching data from an API

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Form from './components/Form';
+import Users from './components/Users';
 function App() {
   return (
     <div className="App">
       <Form />
+      <Users />
     </div>
   );
 }
